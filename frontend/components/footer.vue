aded04ad5b1f4a89b8863410de9f065dd81ac673
MrAdib <Mr.John.Adib@gmail.com>
2024-06-09 20:51:03 +0100

    add link to github in footer

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -4,6 +4,10 @@
       <div>
         Created by <NuxtLink target="_blank" href="https://resume.mradib.com" class="text-emerald-800">MrAdib</NuxtLink>
       </div>
+      <div>
+        <NuxtLink target="_blank" href="https://github.com/JohnAdib/suggestion-box/"
+          class="text-gray-800 hover:text-gray-600">View the Source on GitHub</NuxtLink>
+      </div>
     </div>
   </footer>
 </template>
